d4b3a66ee938c9561f491971e43e36b999dfefcd
takehiro <takehiro.watanabe@globis.co.jp>
2018-07-23 15:45:58 +0900

    Initial Commit

---
+++ reception-desk-iphone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reception-desk-iphone
+//
+//  Created by Watanabe Takehiro on 2018/07/23.
+//  Copyright © 2018 Watanabe Takehiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
